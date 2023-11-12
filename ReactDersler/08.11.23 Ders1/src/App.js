b84ac92421337fedb66c0e3de31e54ae074c38db
iremnisagundogdu <97432880+iremnisagundogdu@users.noreply.github.com>
2023-11-12 10:27:10 +0300

    Counter
    React UseState  kullanımı öğrenildi.
    "Counter.JS" adında functional component oluşturuldu ve 'Counter' uygulaması yapıldı.

---
+++ ReactDersler/08.11.23 Ders1/src/App.js
@@ -0,0 +1,16 @@
+import Counter from './Counter.js'
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-3">
+          <Counter/>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
