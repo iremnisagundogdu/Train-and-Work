06c5d701a191265f577e9e955c167af2a36157dd
iremnisagundogdu <97432880+iremnisagundogdu@users.noreply.github.com>
2023-11-12 10:19:37 +0300

    Sınıf Çalışması
    React.JS'e giriş  ve konu anlatımı yapıldı. Son derste App.js dosyası içerisinde JSX formatında basit  bir site içeriği yapılması istendi.

---
+++ ReactDersler/07.11.23 Sınıf Çalışması/src/App.js
@@ -0,0 +1,81 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     
+     <header>
+       
+       <div>
+        <img>LOGO</img>
+       </div>
+
+        <div>
+          <ul>
+            <li><>Anasayfa</></li>
+            <li>Hakkımızda</li>
+            <li>Giriş Yap</li>
+            <li>İletişim</li>
+          </ul>
+        </div>
+
+        <div class="main">
+         
+         <div class="kategori">
+          
+         </div>
+
+        <div class="Ürünler">
+
+        <div class="etkinlikler">
+        
+          <div class="etknlik" style="background-color: lightseagreen">
+            <div class="etknlk-image etk-1"></div>
+            <h2>KİTAP</h2>
+          </div>
+      
+
+          <div class="etknlk" style="background-color: lightgreen">
+            <div class="etknlk-image etk-3"></div>
+            <h2>OYUN KONSOLU</h2>
+          </div>
+        
+
+       
+          <div class="etknlk" style="background-color: plum">
+            <div class="etknlk-image etk-2"></div>
+            <h2>DANS MALZEMELERİ</h2>
+          </div>
+      
+      </div>
+
+      <div class="etkinlikler">
+       
+          <div class="etknlk" style="background-color: coral">
+            <div class="etknlk-image etk-5"></div>
+            <h2>RESİM MALZEMELERİ</h2>
+          </div>
+        
+
+          <div class="etknlk" style="background-color: lightskyblue">
+            <div class="etknlk-image etk-4"></div>
+            <h2>MÜZİK ALETLERİ</h2>
+          </div>
+        
+          <div class="etknlk" style="background-color: khaki">
+            <div class="etknlk-image etk-6"></div>
+            <h2>BULMACA ÇÖZELİM</h2>
+          </div>
+        
+      </div>
+        </div>
+        </div>
+    
+    </header>
+     
+    </div>
+  );
+}
+
+export default App;
