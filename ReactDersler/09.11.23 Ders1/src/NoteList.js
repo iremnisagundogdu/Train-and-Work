485425ce4170577fa9818b4278b73e827db146db
iremnisagundogdu <97432880+iremnisagundogdu@users.noreply.github.com>
2023-11-12 12:15:40 +0300

    NoteList (Silme Özelliği)
    Eklenen görevin yanında 'Sil' butonu oluşturulup butona tıklanıldığında görevin silinmesi sağlandı.

---
+++ ReactDersler/09.11.23 Ders1/src/NoteList.js
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+
+function NoteList() {
+  const [list, setList] = useState([
+    {
+      id: 0,
+      description: "açıklama",
+      title: "başlık",
+    },
+    {
+      id: 1,
+      description: "açıklama",
+      title: "başlık",
+    },
+  ]);
+  const [newTitle, setNewTitle] = useState("");
+  const [newDesc, setNewDesc] = useState("");
+  const [count, setCount] = useState(1);
+
+  const Ekle = () => {
+    setCount(count + 1);
+
+    setList([...list, { id: count, title: newTitle, description: newDesc }]);
+    setNewTitle("");
+    setNewDesc("");
+  };
+  return (
+    <div>
+      <input
+        type="text"
+        value={newTitle}
+        onChange={(e) => setNewTitle(e.target.value)}
+      />
+      <input
+        type="text"
+        value={newDesc}
+        onChange={(e) => setNewDesc(e.target.value)}
+      />
+      <button onClick={Ekle}>Ekle</button>
+
+      {list.map((e, index) => {
+        return (
+          <div key={index}>
+            <ul>
+              <li>{e.title}</li>
+              <li>{e.description}</li>
+            </ul>
+            <button onClick={()=>setList(list.filter(a=>a.id!=e.id))}>Sil</button>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+export default NoteList;
