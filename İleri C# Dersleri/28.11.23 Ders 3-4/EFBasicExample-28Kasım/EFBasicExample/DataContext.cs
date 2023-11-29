cec6c78482c5e3e51b8aa88f60fdce42ebeb7328
iremnisagundogdu <97432880+iremnisagundogdu@users.noreply.github.com>
2023-11-29 10:50:34 +0300

    Entity Framework
    Veri tabanı oluşturup ayağa kaldırıldı. Veri ekleme, veri güncelleme ve veri silme işlemleri gerçekleştirildi.

---
+++ İleri C# Dersleri/28.11.23 Ders 3-4/EFBasicExample-28Kasım/EFBasicExample/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace EFBasicExample
+{
+    internal class DataContext:DbContext
+    {
+        public DataContext():base("DatabaseConnection") { } // Veri tabanıyla iletişim halindeyim.
+
+        public DbSet<Category>Category { get; set; }
+
+        public DbSet<Product>Product { get; set; }
+    }
+}
